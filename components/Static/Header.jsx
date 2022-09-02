c0f5af4af81b30f5ea5a37884d83697ff688f6e6
Łuîxsα <73739069+Luixsa@users.noreply.github.com>
2022-09-02 17:44:28 +0300

    Update Header.jsx

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -167,7 +167,7 @@ const Header = ({ $, NavItems }) => {
                             </Transition>
                         </Menu>
                         <Link href="https://discord.com/api/oauth2/authorize?client_id=1014341550462996561&permissions=8&scope=bot%20applications.commands">
-                            <a className="w-auto flex items-center justify-center shadow-lg gap-x-2 shadow-amber-600/20 rounded-xl py-2.5 font-medium px-7 bg-gradient-to-tl from-amber-500 to-amber-700 text-white  hover:opacity-80 transition duration-200 target=_blank">
+                            <a className="w-auto flex items-center justify-center shadow-lg gap-x-2 shadow-amber-600/20 rounded-xl py-2.5 font-medium px-7 bg-gradient-to-tl from-amber-500 to-amber-700 text-white  hover:opacity-80 transition duration-200">
                                Invite
                             </a>
                </Link>

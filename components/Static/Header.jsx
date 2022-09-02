aa23db2d3585e20a2a7758c77cedb8865740a98a
Łuîxsα <73739069+Luixsa@users.noreply.github.com>
2022-09-02 18:43:50 +0300

    Update Header.jsx

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -13,7 +13,7 @@ const MobileNavbar = ({ open, setOpen, NavItems }) => {
 
                 <div className="flex justify-between border-b border-white/20 items-center px-5 py-4">
                     <div className="flex items-center">
-                        <img width="32" className="rounded-full icon-glow" src="/img/logo.jpg" />
+                        <img width="32" className="rounded-full icon-glow" src="https://cdn.discordapp.com/attachments/997187785154908311/1015278632232570911/logo.jpg" />
                         <p className=" font-extrabold ml-1 text-2xl">Miyano Bot</p>
                     </div>
                     <button onClick={() => setOpen(!open)}><i className="cursor-pointer fa fa-times text-xl mr-2" /></button>
@@ -76,7 +76,7 @@ const Header = ({ $, NavItems }) => {
                 <div className="max-w-7xl px-5 mx-auto py-5 flex items-center justify-between">
                     <div className="flex items-center space-x-6">
                         <div className="flex items-center space-x-3">
-                            <img src="/img/logo2.png" 
+                            <img src="https://cdn.discordapp.com/attachments/997187785154908311/1015278632232570911/logo.jpg" 
                                 className={`
                                     rounded-full 
                                 `}

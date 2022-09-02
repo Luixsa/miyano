1f87ad65bbc912ee34735028a8d80999e8957d13
Łuîxsα <73739069+Luixsa@users.noreply.github.com>
2022-09-02 18:35:27 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@ export default function Index() {
     <>
       <div className="py-20 mb-30 max-w-3xl mx-auto">
         <div className="flex w-full justify-center">
-      <img width="500" src="https://cdn.discordapp.com/attachments/997188225040928799/1015283226199195708/kindpng_1188785.png" />
+      <img width="500" src="https://cdn.discordapp.com/attachments/997188225040928799/1015283774541529249/kindpng_125085.png" />
       </div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
           Miyano Bot

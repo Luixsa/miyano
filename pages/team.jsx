bf2d95aa76436d2e4a5f93b9fba02759ecbf381d
Łuîxsα <73739069+Luixsa@users.noreply.github.com>
2022-09-04 11:16:25 +0300

    Update team.jsx

--- pages/team.jsx
+++ pages/team.jsx
@@ -34,7 +34,7 @@ export default function Partners() {
                     <div className="flex flex-col justify-center text-white rounded">
                         <a href="https://discord.gg/J69KSqvGeq">
                             <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://tenor.com/view/tokyo-ghoul-anime-kaneki-ken-reflection-eye-patch-gif-17396765" />
+                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/attachments/997188225040928799/1015898018391793664/tokyo-ghoul-anime.gif" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">Founder</h1>        
                                 <div className="flex items-center mt-1">

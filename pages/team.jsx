158031883459b7d9987ccc5171886318f8499088
Łuîxsα <73739069+Luixsa@users.noreply.github.com>
2022-09-11 04:32:28 +0300

    Update team.jsx

--- pages/team.jsx
+++ pages/team.jsx
@@ -18,7 +18,7 @@ export default function Partners() {
             <div className="flex flex-col justify-center text-white rounded">
             <a href="https://discord.gg/J69KSqvGeq">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/attachments/997187785154908311/1015836301037142118/Man_PP_Gif_15.gif" />
+                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/attachments/997187785154908311/1018332839676223508/neymar-jr.gif" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">Founder</h1>        
                                 <div className="flex items-center mt-1">

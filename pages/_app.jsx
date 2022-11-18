5bf22c0a492171a4b9394944e0a5fe324461c9b8
Łuîxsα <73739069+Luixsa@users.noreply.github.com>
2022-11-18 14:02:08 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -37,7 +37,7 @@ export default function AwardApp({ Component, pageProps }) {
       name: "Support",
       icon: "fab fa-discord",
       activeIcon: "fab fa-discord",
-      href: "https://discord.gg/J69KSqvGeq",
+      href: "https://discord.gg/RDNNXqdVab",
     },
     {
       link: true,
